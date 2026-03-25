27c54388fe9f3a1f611c0c89dbfbede7907d2fef
Arya Putra <aryafaiz108@gmail.com>
2026-03-25 07:12:49 +0700

    fix

--- web/app/page.js
+++ web/app/page.js
@@ -6,11 +6,6 @@ import CurtainWrapper from "./_components/CurtainWrapper";
 import SmoothScroller  from "./_components/SmoothScroller";
 import { getEvents }   from "@/lib/directus";
 
-<<<<<<< HEAD
-export const dynamic = 'force-dynamic';
-
-=======
->>>>>>> eb0263c401aa85307dadef88c35de9b3464fcf12
 export default async function Page() {
   const events = await getEvents();
   console.log("Daftar Event dari Directus:", events);
