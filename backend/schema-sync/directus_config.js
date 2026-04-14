7c75af6a1319c07e1de5083063815136e7079994
Arya Putra <aryafaiz108@gmail.com>
2026-04-14 19:47:30 +0700

    sync all database

--- backend/schema-sync/directus_config.js
+++ backend/schema-sync/directus_config.js
@@ -1,13 +1,5 @@
 /**
- * This file contains the default configuration for the schema exporter.
- * 
- * Some possibly sensitive collections are commented out, remove the comments and add filters if needed
- * 
- * Uncomment the collection you want to export.
- * 
- * These are just some sensible settings, but you might not want to export everything
- * 
- * Add custom collections to the syncCustomCollections object in the config.js file.
+ * Konfigurasi sinkronisasi data untuk System Collections bawaan Directus
  */
 export const syncDirectusCollections = {
 	directus_folders: {
@@ -18,7 +10,7 @@ export const syncDirectusCollections = {
 			sort: ['parent', 'id'],
 		},
 	},
-	/* directus_files: {
+	directus_files: {
 		watch: ['files'],
 		excludeFields: [],
 		query: {
@@ -28,7 +20,7 @@ export const syncDirectusCollections = {
 				},
 			}
 		},
-	},*/
+	},
 	directus_roles: {
 		watch: ['roles'],
 		linkedFields: ['parent'],
@@ -61,19 +53,12 @@ export const syncDirectusCollections = {
 			},
 		},
 	},
-	/* directus_users: {
+	directus_users: {
 		watch: ['users'],
 		excludeFields: ['avatar'],
 		query: {
-			filter: {
-				id: {
-					_in: [], // insert id of users you want to export
-				},
-			},
-			limit: 1,
-			sort: ['d]
+			sort: ['id']
 		},
-		/* // Uncomment this to export the password
 		onExport: async (item, itemsSrv) => {
 			if (item.password && item.password === '**********') {
 				const user = await itemsSrv.knex.select('password').from('directus_users').where('id', item.id).first();
@@ -81,24 +66,20 @@ export const syncDirectusCollections = {
 					item.password = user.password;
 				}
 			}
-		
 			return item;
 		},
-		// And then to import the password
 		onImport: async (item, itemsSrv) => {
 			if (item.password && item.password.startsWith('$argon')) {
 				await itemsSrv.knex('directus_users').update('password', item.password).where('id', item.id);
 				item.password = '**********';
 			}
-		
 			return item;
 		},
-	},*/
+	},
 	directus_settings: {
 		watch: ['settings'],
 		excludeFields: [
 			'project_url',
-			// always keep these 3 excluded
 			'mv_hash', 'mv_ts', 'mv_locked',
 		],
 	},
@@ -127,7 +108,7 @@ export const syncDirectusCollections = {
 			}
 		}
 	},
-	/*directus_flows: {
+	directus_flows: {
 		watch: ['flows'],
 		excludeFields: ['operations', 'user_created', 'date_created'],
 		query: {
@@ -145,7 +126,7 @@ export const syncDirectusCollections = {
 				flow: { trigger: { _neq: 'webhook' } },
 			},
 		},
-	},*/
+	},
 	directus_translations: {
 		watch: ['translations'],
 		excludeFields: ['id'],
